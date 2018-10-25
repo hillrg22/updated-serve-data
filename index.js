839d426a79ac0f1eff58423d3f6879c3255d57b7
hillrg22 <hillrg22@gmail.com>
2018-10-25 15:28:58 -0600

    added app.listen, created cohorts variable to import cohorts.js.  wrote .get

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express()
 const cors = require ('cors')
 const parser = require('body-parser')
 
-
-
 app.use(cors())
 app.use(parser.json())
 app.use(parser.urlencoded({ extended: false}))
+
+let port = process.env.PORT || 3000
+
+
+const cohorts = require('./cohorts.js')
+
+// console.log(cohorts)
+
+app.get('/', (req,res,next) => {
+  res.send('YOOOOOOOOOO')
+})
+
+app.listen(port, () => console.log(`serve-data server running on port ${port}`))
